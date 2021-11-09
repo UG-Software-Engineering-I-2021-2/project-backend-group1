ba23f9c461a9a1f72320336b34852fb8c4ab2f19
jneirar <jorge.neira@utec.edu.pe>
2021-11-09 11:54:19 -0500

    Fix variable names

--- src/main/java/controller/CoursesUsernameController.java
+++ src/main/java/controller/CoursesUsernameController.java
@@ -1,18 +1,9 @@
 package controller;
 
-import business.CourseService;
 import business.UserService;
 import data.entities.Seccion;
 import data.entities.User;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import data.entities.Curso;
-import data.entities.Seccion;
-import data.entities.User;
-import org.checkerframework.checker.units.qual.A;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,24 +13,20 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 
-import javax.persistence.criteria.CriteriaBuilder;
-
 class Course{
     private String name;
     private String code;
-    Course(String _name, String _code){
-        name = _name;
-        code = _code;
+
+    public Course(String name, String code) {
+        this.name = name;
+        this.code = code;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
     public void setName(String name) {
         this.name = name;
@@ -54,24 +41,24 @@ class Course{
     }
 }
 
-class Courses_Username_Body {
-    private String semestre;
-    public String getSemestre() {return semestre;}
-    public void setSemestre(String semestre) {this.semestre = semestre;}
+class CoursesUsernameBody {
+    private String semester;
+    public String getSemester() {return semester;}
+    public void setSemestre(String semester) {this.semester = semester;}
 }
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-public class Courses_Username_Controller {
-    private TokenValidator tokenValidator = new TokenValidator();
+public class CoursesUsernameController {
+    private final TokenValidator tokenValidator = new TokenValidator();
 
-    private Gson gson = new Gson();
+    private final Gson gson = new Gson();
 
     @Autowired
     private UserService userService;
 
     @PostMapping(value = "/courses_username")
-    public ResponseEntity<String> courses_username_controller(@RequestHeader(value="Authorization") String authorization, @RequestBody Courses_Username_Body courses_username_body) throws JSONException, GeneralSecurityException, IOException {
+    public ResponseEntity<String> coursesUsernameController(@RequestHeader(value="Authorization") String authorization, @RequestBody CoursesUsernameBody coursesUsernameBody) throws JSONException, GeneralSecurityException, IOException {
         System.out.println("TEST COURSE");
 
         Payload payload = tokenValidator.ValidateTokenAndGetPayload(authorization);
@@ -85,14 +72,14 @@ public class Courses_Username_Controller {
         ArrayList<Course> cursos = new ArrayList<>();
 
         String email = payload.getEmail();
-        String semestre = courses_username_body.getSemestre();
+        String semester = coursesUsernameBody.getSemester();
         String username = email.substring(0,email.indexOf('@'));
 
         User user = userService.findByUsername(username);
 
         if(user.getRol().toString().equals("Docente")){
-            Set<Seccion> set_secciones = user.getSeccionesDicta(semestre);
-            for(Seccion seccion : set_secciones){
+            Set<Seccion> setSecciones = user.getSeccionesDicta(semester);
+            for(Seccion seccion : setSecciones){
                 Course curso = new Course(seccion.getCursoSeccion().getNombre(), seccion.getCursoSeccion().getCodCurso());
                 cursos.add(curso);
             }
---
+++ src/main/java/controller/RubricsCourseController.java
@@ -0,0 +1,171 @@
+package controller;
+
+import business.CourseService;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.gson.Gson;
+import data.entities.*;
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+class Rubric{
+    private String code;
+    private String state;
+    private String evaluation;
+    private String date;
+    private String week;
+    private String evidence;
+    private String activity;
+    private String canEdit;
+    private String students;
+
+    public Rubric(String code, String state, String evaluation, String date, String week, String evidence, String activity) {
+        this.code = code;
+        this.state = state;
+        this.evaluation = evaluation;
+        this.date = date;
+        this.week = week;
+        this.evidence = evidence;
+        this.activity = activity;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(String evaluation) {
+        this.evaluation = evaluation;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+
+    public String getEvidence() {
+        return evidence;
+    }
+
+    public void setEvidence(String evidence) {
+        this.evidence = evidence;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public String getCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(String canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public String getStudents() {
+        return students;
+    }
+
+    public void setStudents(String students) {
+        this.students = students;
+    }
+}
+
+class RubricsCourseBody {
+    private String semester;
+    private String courseCode;
+    public String getSemester() {return semester;}
+    public String getCoursecode() {return courseCode;}
+}
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+public class RubricsCourseController {
+    private final TokenValidator tokenValidator = new TokenValidator();
+
+    private final Gson gson = new Gson();
+
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping("/rubrics_course")
+    public ResponseEntity<String> rubricsCourseController(@RequestHeader(value="Authorization") String authorization, @RequestBody RubricsCourseBody rubricsCourseBody) throws JSONException, GeneralSecurityException, IOException {
+        GoogleIdToken.Payload payload = tokenValidator.ValidateTokenAndGetPayload(authorization);
+
+        if(payload == null){
+            HashMap<String, String> errorMap = new HashMap<>();
+            errorMap.put("error", "token not verified");
+            return  ResponseEntity.status(404).body(gson.toJson(errorMap));
+        }
+
+        ArrayList<Rubric> response = new ArrayList<>();
+
+        String courseCode = rubricsCourseBody.getCoursecode();
+        String semester = rubricsCourseBody.getSemester();
+        Curso course = courseService.findOneByCodCurso(courseCode);
+        Set<RubricaBase> setRubricsBase = course.getRubricasBase();
+        for(RubricaBase rubricaBase : setRubricsBase){
+            Set<Rubrica> setRubrics = rubricaBase.getRubricas();
+            for(Rubrica rubrica : setRubrics){
+                if (rubrica.getSemestre().equals(semester)) {
+                    Rubric rubric = new Rubric(
+                            rubricaBase.getCodRubrica(),
+                            rubrica.getEstado(),
+                            rubricaBase.getEvaluacion(),
+                            rubrica.getFecha().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                            "Semana " + rubricaBase.getSemana(),
+                            rubricaBase.getEvidencia(),
+                            rubricaBase.getActividadBase()
+                    );
+                    rubric.setCanEdit("1");
+                    rubric.setStudents("0");
+                    response.add(rubric);
+                }
+            }
+        }
+        //map.forEach((k,v) -> System.out.println("Key: " + k + ": Value: " + v));
+
+        System.out.println(gson.toJson(response));
+        return ResponseEntity.status(200).body(gson.toJson(response));
+    }
+}
+

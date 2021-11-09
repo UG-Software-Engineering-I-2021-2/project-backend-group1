ca354e9bbbe502e49f2c19e75f06842983ca47c4
jneirar <jorge.neira@utec.edu.pe>
2021-11-08 21:48:13 -0500

    Fix course

--- src/main/java/controller/Courses_Username_Controller.java
+++ src/main/java/controller/Courses_Username_Controller.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;

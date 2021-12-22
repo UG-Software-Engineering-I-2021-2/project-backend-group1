141e69b4c7f1b145388d5f73ab6fe48fd210230c
renato <luis.carbajal@utec.edu.pe>
2021-12-22 02:41:30 -0500

    updated entities

--- src/test/java/software/group1/project/ProjectApplicationTests.java
+++ src/test/java/software/group1/project/ProjectApplicationTests.java
@@ -1,12 +1,15 @@
 package software.group1.project;
 
+import config.endpoint_classes.rubric.Rubric;
 import config.enums.Role;
+import config.enums.State;
 import data.entities.*;
-import data.entities.composite_keys.SeccionPK;
+import data.entities.composite_keys.*;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -107,19 +110,287 @@ public class ProjectApplicationTests {
 		String codcurso = "EN01";
 		String nombre = "curso";
 		Set<Seccion> secciones = new HashSet<>();
-		Set<RubricaBase> rubricaBase = new HashSet<>();
+		Set<RubricaBase> rubricasBase = new HashSet<>();
 		Set<Carrera> carreras = new HashSet<>();
 
 		c.setCodCurso(codcurso);
 		c.setNombre(nombre);
 		c.setSecciones(secciones);
-		c.setRubricasBase(rubricaBase);
+		c.setRubricasBase(rubricasBase);
 		c.setCarrerasPertenece(carreras);
 
 		Assert.assertEquals(c.getCodCurso(), codcurso);
 		Assert.assertEquals(c.getNombre(), nombre);
 		Assert.assertEquals(c.getSecciones(), secciones);
-		Assert.assertEquals(c.getRubricasBase(), rubricaBase);
+		Assert.assertEquals(c.getRubricasBase(), rubricasBase);
 		Assert.assertEquals(c.getCarrerasPertenece(), carreras);
+	}
+
+	@Test
+	public void CarreraTest() {
+		Carrera c = new Carrera();
+		Long id = 0L;
+		String nombre = "carrera";
+		Set<Alumno> alumnos = new HashSet<>();
+		Set<Competencia> competencias = new HashSet<>();
+		Set<Curso> cursos = new HashSet<>();
+
+		c.setId(id);
+		c.setNombre(nombre);
+		c.setAlumnos(alumnos);
+		c.setCompetencias(competencias);
+		c.setCursosPertenece(cursos);
+
+		Assert.assertEquals(c.getId(), id);
+		Assert.assertEquals(c.getNombre(), nombre);
+		Assert.assertEquals(c.getAlumnos(), alumnos);
+		Assert.assertEquals(c.getCompetencias(), competencias);
+		Assert.assertEquals(c.getCursosPertenece(), cursos);
+	}
+
+	@Test
+	public void AlumnoTest() {
+		Alumno a = new Alumno();
+		String codalumno = "0001";
+		Carrera carrera = new Carrera();
+		String nombre = "alumno";
+		Set<Lleva> lleva = new HashSet<>();
+		Set<Evalua> evalua = new HashSet<>();
+
+		a.setCodAlumno(codalumno);
+		a.setCarreraAlumno(carrera);
+		a.setNombre(nombre);
+		a.setLleva(lleva);
+		a.setEvalua(evalua);
+
+		Assert.assertEquals(a.getCodAlumno(), codalumno);
+		Assert.assertEquals(a.getCarreraAlumno(), carrera);
+		Assert.assertEquals(a.getNombre(), nombre);
+		Assert.assertEquals(a.getLleva(), lleva);
+		Assert.assertEquals(a.getEvalua(), evalua);
+	}
+
+	@Test
+	public void CompetenciaTest() {
+		Competencia c = new Competencia();
+		String codcompetencia = "C01";
+		Carrera carrera = new Carrera();
+		String descripcion = "descripcion";
+		Set<RubricaBase> rubricasBase = new HashSet<>();
+
+		c.setCodCompetencia(codcompetencia);
+		c.setCarreraCompetencia(carrera);
+		c.setDescripcion(descripcion);
+		c.setRubricas(rubricasBase);
+
+		Assert.assertEquals(c.getCodCompetencia(), codcompetencia);
+		Assert.assertEquals(c.getCarreraCompetencia(), carrera);
+		Assert.assertEquals(c.getDescripcion(), descripcion);
+		Assert.assertEquals(c.getRubricas(), rubricasBase);
+	}
+
+	@Test
+	public void EvaluaTest() {
+		Evalua e = new Evalua();
+
+		EvaluaPK ePK = new EvaluaPK();
+		String codalumno = "0001";
+		RubricaPK rubricaPK = new RubricaPK();
+
+		ePK.setCodAlumno(codalumno);
+		ePK.setRubricaPK(rubricaPK);
+
+		Assert.assertEquals(ePK.getCodAlumno(), codalumno);
+		Assert.assertEquals(ePK.getRubricaPK(), rubricaPK);
+
+		String calificacionAlumno = "0";
+		String calificacionCompetencia = "1";
+		boolean evaluacionTotal = false;
+		Alumno alumno = new Alumno();
+		Rubrica rubrica = new Rubrica();
+
+		e.setEvaluaPK(ePK);
+		e.setCalificacionAlumno(calificacionAlumno);
+		e.setCalificacionCompetencia(calificacionCompetencia);
+		e.setEvaluacionTotal(evaluacionTotal);
+		e.setAlumnoEvalua(alumno);
+		e.setRubricaEvalua(rubrica);
+
+		Assert.assertEquals(e.getEvaluaPK(), ePK);
+		Assert.assertEquals(e.getCalificacionAlumno(), calificacionAlumno);
+		Assert.assertEquals(e.getCalificacionCompetencia(), calificacionCompetencia);
+		Assert.assertEquals(e.isEvaluacionTotal(), evaluacionTotal);
+		Assert.assertEquals(e.getAlumnoEvalua(), alumno);
+		Assert.assertEquals(e.getRubricaEvalua(), rubrica);
+	}
+
+	@Test
+	public void LlevaTest() {
+		Lleva l = new Lleva();
+
+		LlevaPK lPK = new LlevaPK();
+		String codalumno = "0001";
+		SeccionPK seccionPK = new SeccionPK();
+
+		lPK.setCodAlumno(codalumno);
+		lPK.setSeccionPK(seccionPK);
+
+		Assert.assertEquals(lPK.getCodAlumno(), codalumno);
+		Assert.assertEquals(lPK.getSeccionPK(), seccionPK);
+
+		Short ciclo = 2;
+		Alumno alumno = new Alumno();
+		Seccion seccion = new Seccion();
+
+		l.setLlevaPK(lPK);
+		l.setCiclo(ciclo);
+		l.setAlumnoLleva(alumno);
+		l.setSeccionLleva(seccion);
+
+		Assert.assertEquals(l.getLlevaPK(), lPK);
+		Assert.assertEquals(l.getCiclo(), ciclo);
+		Assert.assertEquals(l.getAlumnoLleva(), alumno);
+		Assert.assertEquals(l.getSeccionLleva(), seccion);
+	}
+
+	@Test
+	public void RubricaTest() {
+		Rubrica r = new Rubrica();
+
+		RubricaPK rPK = new RubricaPK();
+		String semestre = "2021 - 2";
+		RubricaBasePK rubricaBasePK = new RubricaBasePK();
+
+		rPK.setSemestre(semestre);
+		rPK.setRubricaBasePK(rubricaBasePK);
+
+		Assert.assertEquals(rPK.getSemestre(), semestre);
+		Assert.assertEquals(rPK.getRubricaBasePK(), rubricaBasePK);
+
+		Short dimensiones = 3;
+		LocalDate fecha = LocalDate.of(2020, 1, 8);
+		State estado = State.SinAsignar;
+		String descriptores = "d1,d2,d3";
+		String actividad = "actividad";
+		String titulo = "titulo";
+		RubricaBase rubricaBase = new RubricaBase();
+		Set<Evalua> evalua = new HashSet<>();
+		Set<Seccion> secciones = new HashSet<>();
+
+		r.setRubricaPK(rPK);
+		r.setDimensiones(dimensiones);
+		r.setFecha(fecha);
+		r.setEstado(estado);
+		r.setDescriptores(descriptores);
+		r.setActividad(actividad);
+		r.setTitulo(titulo);
+		r.setRubricaBase(rubricaBase);
+		r.setEvalua(evalua);
+		r.setSeccionesRubricFinish(secciones);
+
+		Assert.assertEquals(r.getRubricaPK(), rPK);
+		Assert.assertEquals(r.getDimensiones(), dimensiones);
+		Assert.assertEquals(r.getFecha(), fecha);
+		Assert.assertEquals(r.getEstado(), estado);
+		Assert.assertEquals(r.getDescriptores(), descriptores);
+		Assert.assertEquals(r.getActividad(), actividad);
+		Assert.assertEquals(r.getTitulo(), titulo);
+		Assert.assertEquals(r.getRubricaBase(), rubricaBase);
+		Assert.assertEquals(r.getEvalua(), evalua);
+		Assert.assertEquals(r.getSeccionesRubricFinish(), secciones);
+		Assert.assertEquals(r.getSemestre(), semestre);
+	}
+
+	@Test
+	public void RubricaBaseTest() {
+		RubricaBase r = new RubricaBase();
+
+		RubricaBasePK rPK = new RubricaBasePK();
+		String codrubrica = "R01";
+		String codcurso = "EN01";
+		String codcompetencia = "C01";
+
+		rPK.setCodRubrica(codrubrica);
+		rPK.setCodCurso(codcurso);
+		rPK.setCodCompetencia(codcompetencia);
+
+		Assert.assertEquals(rPK.getCodRubrica(), codrubrica);
+		Assert.assertEquals(rPK.getCodCurso(), codcurso);
+		Assert.assertEquals(rPK.getCodCompetencia(), codcompetencia);
+
+		String actividadBase = "actividad base";
+		Short semana = 7;
+		Short nivel = 1;
+		String criterioDesempeno = "criterio de desempeño";
+		String evaluacion = "evaluacion";
+		String evidencia = "evidencia";
+		String tituloBase = "titulo base";
+		Curso curso = new Curso();
+		Competencia competencia = new Competencia();
+		Set<Rubrica> rubricas = new HashSet<>();
+
+		Rubrica rubrica1 = new Rubrica();
+		RubricaPK rubricaPK1 = new RubricaPK();
+		rubricaPK1.setSemestre("2021 - 2");
+		rubrica1.setRubricaPK(rubricaPK1);
+
+		Rubrica rubrica2 = new Rubrica();
+		RubricaPK rubricaPK2 = new RubricaPK();
+		rubricaPK2.setSemestre("2021 - 1");
+		rubrica2.setRubricaPK(rubricaPK2);
+
+		Rubrica rubrica3 = new Rubrica();
+		RubricaPK rubricaPK3 = new RubricaPK();
+		rubricaPK3.setSemestre("2021 - 2");
+		rubrica3.setRubricaPK(rubricaPK3);
+
+		rubricas.add(rubrica1);
+		rubricas.add(rubrica2);
+		rubricas.add(rubrica3);
+
+		r.setRubricaPK(rPK);
+		r.setActividadBase(actividadBase);
+		r.setSemana(semana);
+		r.setNivel(nivel);
+		r.setCriterioDesempeno(criterioDesempeno);
+		r.setEvaluacion(evaluacion);
+		r.setEvidencia(evidencia);
+		r.setTituloBase(tituloBase);
+		r.setCursoRubrica(curso);
+		r.setCompetenciaRubrica(competencia);
+		r.setRubricas(rubricas);
+
+		Assert.assertEquals(r.getRubricaPK(), rPK);
+		Assert.assertEquals(r.getActividadBase(), actividadBase);
+		Assert.assertEquals(r.getSemana(), semana);
+		Assert.assertEquals(r.getNivel(), nivel);
+		Assert.assertEquals(r.getCriterioDesempeno(), criterioDesempeno);
+		Assert.assertEquals(r.getEvaluacion(), evaluacion);
+		Assert.assertEquals(r.getEvidencia(), evidencia);
+		Assert.assertEquals(r.getTituloBase(), tituloBase);
+		Assert.assertEquals(r.getCursoRubrica(), curso);
+		Assert.assertEquals(r.getCompetenciaRubrica(), competencia);
+		Assert.assertEquals(r.getRubricas(), rubricas);
+		Assert.assertEquals(r.getCodRubrica(), codrubrica);
+
+		Assert.assertEquals(r.getRubricas("2021 - 2").size(), 2);
+		Assert.assertEquals(r.getRubrica("2021 - 1"), rubrica2);
+	}
+
+	// enums tests
+
+	@Test
+	public void StateTest() {
+		State sinAsignar = State.SinAsignar;
+		State aprobacionPendientes = State.AprobacionPendiente;
+		State disponibleParaCalificar = State.DisponibleParaCalificar;
+		State fueraDeFecha = State.FueraDeFecha;
+		State cumplidos = State.Cumplidos;
+
+		Assert.assertEquals(sinAsignar.toString(), "Sin asignar");
+		Assert.assertEquals(aprobacionPendientes.toString(), "Aprobacion pendiente");
+		Assert.assertEquals(disponibleParaCalificar.toString(), "Disponible para calificar");
+		Assert.assertEquals(fueraDeFecha.toString(), "Fuera de fecha");
+		Assert.assertEquals(cumplidos.toString(), "Cumplidos");
 	}
 }
